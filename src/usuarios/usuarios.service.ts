6e011687bcf8a3389d6dc032d1555ad2f4f2e4bd
eliascharadia <eliascharadia01@gmail.com>
2026-06-20 22:04:45 -0300

    add: servicio con metodos crear, validar usuarios

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Usuario, UsuarioDocument  } from './entities/usuario.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsuariosService {}
+export class UsuariosService {
+    constructor(
+    @InjectModel(Usuario.name) private usuarioModel: Model<UsuarioDocument>,
+  ) {}
+
+  async crear(datos: Partial<Usuario>): Promise<UsuarioDocument> {
+    const nuevoUsuario = new this.usuarioModel(datos);
+    return nuevoUsuario.save();
+  }
+
+  async buscarPorCorreoOUsuario(identificador: string): Promise<UsuarioDocument | null> {
+    return this.usuarioModel.findOne({
+      $or: [{ correo: identificador }, { nombreUsuario: identificador }],
+    });
+  }
+
+  async existeCorreoOUsuario(correo: string, nombreUsuario: string): Promise<UsuarioDocument | null> {
+    return this.usuarioModel.findOne({
+      $or: [{ correo }, { nombreUsuario }],
+    });
+  }
+}
