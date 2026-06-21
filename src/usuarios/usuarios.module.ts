336c6c00e8b95e020bc9cdb9f772a32da296d759
eliascharadia <eliascharadia01@gmail.com>
2026-06-20 22:03:50 -0300

    add: configuracion del mongooseModule

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Usuario, UsuarioSchema } from './entities/usuario.schema';
 import { UsuariosController } from './usuarios.controller';
 import { UsuariosService } from './usuarios.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Usuario.name, schema: UsuarioSchema }]),
+  ],
   controllers: [UsuariosController],
-  providers: [UsuariosService]
+  providers: [UsuariosService],
+  exports: [UsuariosService, MongooseModule],
 })
 export class UsuariosModule {}
