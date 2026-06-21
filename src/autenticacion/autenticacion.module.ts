b347483a75c00cd7ab645ddc794192c416cc8361
eliascharadia <eliascharadia01@gmail.com>
2026-06-20 22:03:04 -0300

    add: imports necesarios y JWT

--- src/autenticacion/autenticacion.module.ts
+++ src/autenticacion/autenticacion.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsuariosModule } from '../usuarios/usuarios.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { AutenticacionController } from './autenticacion.controller';
 import { AutenticacionService } from './autenticacion.service';
 
 @Module({
+  imports: [
+    UsuariosModule,
+    CloudinaryModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
   controllers: [AutenticacionController],
   providers: [AutenticacionService]
 })
