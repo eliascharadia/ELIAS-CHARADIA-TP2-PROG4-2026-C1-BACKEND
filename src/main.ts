2590480fbe1d8099665278a6bce78eed234f5588
eliascharadia <eliascharadia01@gmail.com>
2026-06-22 16:35:20 -0300

    add: habilitacion de cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,23 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://elias-charadia-tp-2-prog-4-2026-c1.vercel.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
   await app.listen(process.env.PORT ?? 3000);
